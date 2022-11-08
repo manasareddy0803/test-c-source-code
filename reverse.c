4fe7501d066e333784c6b56b07ddffdadfd7b40c
manasareddy0803 <117155370+manasareddy0803@users.noreply.github.com>
2022-11-08 18:15:16 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,37 +1,23 @@
-#include<stdio.h>
-#include<conio.h>
-reverse()
+#include<string.h>
+
+int main(void)
 {
-    FILE *fp;
-    char ch, fname[30], newch[500];
-    int i=0, j, COUNT=0;
-    printf("Enter the filename with extension: ");
-    gets(fname);
-    fp = fopen(fname, "r");
-    if(!fp)
-    {
-        printf("Error in opening the file...\nExiting...");
-        getch();
-        return 0;
+    char mystrg[60];
+    int leng, g;
+
+    // Printing the program name and what the program will do
+    printf("Program in C for reversing a given string \n ");
+    printf("Please insert the string you want to reverse: ");
+
+    // fetch the input string from the user
+    scanf( "%s", mystrg );
+
+    // This will find the length of your string with the help of strlen() function of string.h header file
+    leng = strlen(mystrg);
+
+    // iterate through each and every character of the string for printing it backwards or reverse direction
+    for(g = leng - 1; g >= 0; g--) {
+        printf("%c", mystrg[g]);        
     }
-    printf("\nThe original content is:\n\n");
-    ch = getc(fp);
-    while(ch != EOF)
-    {
-        COUNT++;
-        putchar(ch);
-        newch[i] = ch;
-        i++;
-        ch = getc(fp);
-    }
-    printf("\n\n\n");
-    printf("The content in reverse order is:\n\n");
-    for(j=(COUNT-1); j>=0; j--)
-    {
-        ch = newch[j];
-        printf("%c", ch);
-    }
-    printf("\n");
-    getch();
     return 0;
 }
